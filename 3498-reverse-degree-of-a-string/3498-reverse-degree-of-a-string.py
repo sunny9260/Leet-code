a275dc567a7797aa8c615c53577f20c2ec1e56e3
Sunny.C <153522548+sunny9260@users.noreply.github.com>
2026-09-20 05:54:34 +0530

    Time: 11 ms (20.35%), Space: 19.3 MB (55.57%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reverseDegree(self, s: str) -> int:
+
+        ans, idx = 0, 1
+        for ch in s:
+            ans+= (123 - ord(ch)) * idx
+            idx+= 1
+
+        return ans    
+        
